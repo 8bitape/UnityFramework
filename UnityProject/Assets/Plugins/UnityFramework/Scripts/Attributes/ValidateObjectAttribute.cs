16750476a5214d944a157bd83d96b2a91fb63402
Joe McDonald <joe@8bitape.net>
2019-02-15 17:45:33 +0000

    Added ValidateObjectAttribute

---
+++ UnityProject/Assets/Plugins/UnityFramework/Scripts/Attributes/ValidateObjectAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnityFramework.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+
+    public class ValidateObjectAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return Validator.TryValidateObject(value);
+        }
+    }
+}
