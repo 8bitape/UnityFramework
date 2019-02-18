8fcf8cecc29e7fd6d17c3d44e9194665684d95c3
Joe McDonald <joe@8bitape.net>
2019-02-18 16:02:31 +0000

    Added float extensions

---
+++ UnityProject/Assets/Plugins/UnityFramework/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace UnityFramework.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static void Increment(this float value, float step)
+        {
+            value += step;
+        }
+
+        public static void Increment(this float value, float step, float maxValue)
+        {
+            if (value < maxValue)
+            {
+                value.Increment(step);
+            }
+        }
+
+        public static void Increment(this float value, float step, float maxValue, float wrapValue)
+        {
+            if (value < maxValue)
+            {
+                value.Increment(step);
+            }
+            else
+            {
+                value = wrapValue;
+            }
+        }
+
+        public static void Decrement(this float value, float step)
+        {
+            value.Increment(-step);
+        }
+
+        public static void Decrement(this float value, float step, float minValue)
+        {
+            if (value > minValue)
+            {
+                value.Decrement(step);
+            }
+        }
+
+        public static void Decrement(this float value, float step, float minValue, float wrapValue)
+        {
+            if (value < minValue)
+            {
+                value.Decrement(step);
+            }
+            else
+            {
+                value = wrapValue;
+            }
+        }
+    }
+}
