71055b2b44741831c8d3bd6ae95ed4c5d3daa918
Joe McDonald <joe@8bitape.net>
2019-04-12 17:03:37 +0100

    Added WaitForFixedUpdate() extension method

---
+++ UnityProject/Assets/Plugins/UnityFramework/Examples/Scripts/Extensions/WaitForFixedUpdate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WaitForFixedUpdate : MonoBehaviour
+{    
+    void Awake()
+    {
+        this.enabled = true;
+    }
+
+    void Start()
+    {
+        this.WaitForFixedUpdate(() => this.enabled = false);
+    }
+}
---
+++ UnityProject/Assets/Plugins/UnityFramework/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+public static class MonoBehaviourExtensions
+{
+    public static void WaitForFixedUpdate(this MonoBehaviour monoBehaviour, Action action)
+    {
+        if (!monoBehaviour.gameObject.activeInHierarchy)
+        {
+            return;
+        }
+
+        MainThreadDispatcher.StartFixedUpdateMicroCoroutine(WaitForFixedUpdate(action));
+    }
+
+    private static IEnumerator WaitForFixedUpdate(Action action)
+    {
+        yield return null;
+
+        action.Invoke();
+    }
+}
