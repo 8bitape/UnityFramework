48b4b51b5a862763c4af80e24e067cd98f38b4f5
Joe McDonald <joe@8bitape.net>
2019-04-12 17:03:13 +0100

    Added object pooling

---
+++ UnityProject/Assets/Plugins/UnityFramework/Examples/Scripts/ObjectPools/ObjectPooling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityFramework.ObjectPools;
+
+public class ObjectPooling : MonoBehaviour
+{
+    private Prefab Prefab { get; set; }
+
+    private ObjectPool<Prefab> PrefabObjectPool { get; set; }
+
+    void Start()
+    {
+        this.PrefabObjectPool = new ObjectPool<Prefab>(this.Prefab, 10);
+    }
+
+    void Update()
+    {
+        this.PrefabObjectPool.RentAtPosition(Vector3.zero);
+
+        this.PrefabObjectPool.ReturnAll();
+    }
+}
---
+++ UnityProject/Assets/Plugins/UnityFramework/Scripts/ObjectPools/ObjectPool.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+namespace UnityFramework.ObjectPools
+{
+    public class ObjectPool<TComponent> : UniRx.Toolkit.ObjectPool<TComponent>
+        where TComponent : Component
+    {
+        private TComponent Prefab { get; set; }
+
+        private List<TComponent> Instances { get; set; }
+
+        public ObjectPool(TComponent prefab, int preloadCount)
+        {
+            this.Prefab = prefab;
+
+            this.Instances = new List<TComponent>();
+
+            this.Preload(preloadCount);
+        }
+
+        protected override TComponent CreateInstance()
+        {
+            return GameObject.Instantiate<TComponent>(this.Prefab);
+        }
+
+        public void RentAtPosition(Vector3 position)
+        {
+            var instance = base.Rent();
+
+            instance.transform.position = position;
+        }
+
+        protected override void OnBeforeRent(TComponent instance)
+        {
+            base.OnBeforeRent(instance);
+
+            this.Instances.Add(instance);
+        }
+
+        protected override void OnBeforeReturn(TComponent instance)
+        {
+            base.OnBeforeReturn(instance);
+
+            this.Instances.Remove(instance);
+        }
+
+        public void ReturnAll()
+        {
+            foreach (var instance in this.Instances.AsEnumerable().Reverse())
+            {
+                this.Return(instance);
+            }
+        }
+
+        private void Preload(int preloadCount)
+        {
+            MainThreadDispatcher.StartFixedUpdateMicroCoroutine(this.PreloadCoroutine(preloadCount));
+        }
+
+        private IEnumerator PreloadCoroutine(int preloadCount)
+        {
+            while (this.Count < preloadCount)
+            {
+                var instance = this.CreateInstance();
+
+                this.Return(instance);
+
+                yield return null;
+            }
+        }
+    }
+}
